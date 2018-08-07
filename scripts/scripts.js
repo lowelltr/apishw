4be85073b4c5cb7df7d7f540ffca5802f172de4b
Tremell Lowell <tremelljlowell@gmail.com>
2018-08-07 19:26:50 -0400

    few changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,16 +2,15 @@
 console.log("working");
 $.get(`https://www.reddit.com/r/aww/.json`, (data) =>{
   console.log(data.data.children);
-  let items = data.data.children;
+  let items = data.data.children.slice(0,10);
   items.forEach(function(thing){
     console.log(thing.data.title.thumbnail);
     let url = "https://www.reddit.com" + thing.data.permalink;
     $(".stuff").append(
       "<p>"+thing.data.title+"</p>", 
       "<img src="+"'"+thing.data.thumbnail+"'"+ ">",
-      "<a href='"+ url+ "'>see more</a>"
+      "<a href='"+ url+ "'>see more</a>")
 
-    )
 
    
   
